804bc0a8471c3fad04d8102c0bbee2d89b351ab1
jokamjohn <johnkagga@gmail.com>
2015-06-18 14:23:06 +0300

    Added constructor to prompter class

--- src/me/johnkagga/Prompter.java
+++ src/me/johnkagga/Prompter.java
@@ -4,4 +4,9 @@ package me.johnkagga;
  * Created by JohnKagga on 6/18/2015.
  */
 public class Prompter {
+    private Game mGame;
+
+    public Prompter(Game game) {
+        mGame = game;
+    }
 }
