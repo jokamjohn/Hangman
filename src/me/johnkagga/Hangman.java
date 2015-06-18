ef0340d73c0be03701fb1bb191483a28f6c2b2b9
jokamjohn <johnkagga@gmail.com>
2015-06-18 14:04:57 +0300

    Added applyGuess method

--- src/me/johnkagga/Hangman.java
+++ src/me/johnkagga/Hangman.java
@@ -5,5 +5,7 @@ public class Hangman {
     public static void main(String[] args) {
 	// write your code here
         Game game = new Game("treehouse");
+
+        System.out.print( game.applyGuess('t'));
     }
 }
