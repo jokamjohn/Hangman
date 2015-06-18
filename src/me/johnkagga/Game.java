5b7e8b1c197f80459e0e6dd3a6af29eee1243fb7
jokamjohn <johnkagga@gmail.com>
2015-06-18 13:42:33 +0300

    constructor in game

--- src/me/johnkagga/Game.java
+++ src/me/johnkagga/Game.java
@@ -4,4 +4,9 @@ package me.johnkagga;
  * Created by JohnKagga on 6/18/2015.
  */
 public class Game {
+    private String mAnswer;
+
+    public Game(String answer) {
+        mAnswer = answer;
+    }
 }
--- src/me/johnkagga/Hangman.java
+++ src/me/johnkagga/Hangman.java
@@ -4,5 +4,6 @@ public class Hangman {
 
     public static void main(String[] args) {
 	// write your code here
+        Game game = new Game("treehouse");
     }
 }
