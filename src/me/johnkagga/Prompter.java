d5624a3cfed82a38a5e0171447cdc31fa3f47c71
jokamjohn <johnkagga@gmail.com>
2015-06-18 15:41:44 +0300

    Added promptforguess method

--- src/me/johnkagga/Prompter.java
+++ src/me/johnkagga/Prompter.java
@@ -1,5 +1,7 @@
 package me.johnkagga;
 
+import java.io.Console;
+
 /**
  * Created by JohnKagga on 6/18/2015.
  */
@@ -8,5 +10,15 @@ public class Prompter {
 
     public Prompter(Game game) {
         mGame = game;
+    }
+
+    /*
+    this method gets a guess from the user and checks whether it is a hit or miss
+     */
+    public boolean promptForGuess(){
+        Console console = System.console();
+        String guessAsString = console.readLine("Enter your guess: ");
+        Character guess = guessAsString.charAt(0);
+        return mGame.applyGuess(guess);
     }
 }
